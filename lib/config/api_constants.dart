96062fc8446e181abf7309eb27d62160bc520b64
Dede Fernanda <dedefernanda92@gmail.com>
2026-01-13 15:28:14 +0700

    feat: update ngrok API endpoint for improved connectivity

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -5,7 +5,7 @@ class ApiConstants {
   // static const String _localOther = 'http://127.0.0.1:5000/api';
 
   // kalau mau pakai ngrok, tinggal ganti ini
-  static const String _ngrok = 'http://192.168.48.111:5000/api';
+  static const String _ngrok = 'http://192.168.48.55:5000/api';
 
   static String get baseUrl {
     // ===== PILIH SALAH SATU =====
